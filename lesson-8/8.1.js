24fc979cf655bd67180bc583a55d374c0c6a44fe
2026050 오준석 <162391804+2026050ojs@users.noreply.github.com>
2024-04-03 00:37:25 +0000

    sss

--- lesson-8/8.1.js
+++ lesson-8/8.1.js
@@ -19,5 +19,17 @@ app
         res.send('Hello World!'); // res.send로 서버에서 클라이언트의 응답 발행
     })
     .listen(port, () => { // 3000번 포트로 애플리케이션 셋업
-        console.log(`The Express server is listening on port: ${port}`);
+        console.log(`Express at https://localhost:$:${port}`);
     });
+    .get('/about', (req, res) => { // 
+        /**
+         * Listing 8.2 (p. 140)
+         * 요청 매개변수의 액세스
+         */
+        console.log(req.params);
+        console.log(req.body);
+        console.log(req.url);
+        console.log(req.query);
+
+        res.send('ABOUT ME!'); // res.send로 서버에서 클라이언트의 응답 발행
+    })
